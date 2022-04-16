4fea589abb9f4732d6c06ee4557c6ee902c4ca68
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2022-04-16 18:14:09 -0400

    Edited the categories
    Changed the genre to have 3 text boxes, changed rating to year

--- gui.py
+++ gui.py
@@ -6,17 +6,18 @@ window.geometry("1000x1000")
 initialX = 20 
 initialY = 20
 
+#feels like kinda messy.. consider writing a function that sets up the entire GUI
+
 #Title 
 projectTitle = tk.Label(text = "Film-Inator 5000") #creating the title label 
 projectTitle.pack() #adding the widget to the window, pack sizes the window as small as it can while still fully encompassing the widget 
 
 title = tk.Label(text = "Title:")
-#title = tk.Label(text = "Title:", fg = "white", bg = "black") 
-#title.pack(ipadx = 10, ipady = 10)
 title.place(x = initialX, y =initialY)
 titleEntry = tk.Entry(fg = "black", bg = "white", width = 30)
 titleEntry.pack() #places the titleEntry on the window
 titleEntry.place(x = initialX, y = initialY +20)
+titleInput = titleEntry.get() #prob need to add a button so it knows when to get the text
 
 genre = tk.Label(text = "Genre:")
 genre.pack(ipadx = 10, ipady = 10)
@@ -25,40 +26,50 @@ genreEntry = tk.Entry(fg = "black", bg = "white", width = 30)
 genreEntry.pack() 
 genreEntry.place(x = initialX, y = initialY + 80)
 
-rating = tk.Label(text = "Rating:")
-rating.pack(ipadx = 10, ipady = 10)
-rating.place(x = initialX, y = initialY + 120)
-ratingEntry = tk.Entry(fg = "black", bg = "white", width = 30)
-ratingEntry.pack() 
-ratingEntry.place(x = initialX, y = initialY + 140)
+genreEntry2 = tk.Entry(fg = "black", bg = "white", width = 30)
+genreEntry2.pack()
+genreEntry2.place(x = initialX, y = initialY + 100)
+
+genreEntry3 = tk.Entry(fg = "black", bg = "white", width = 30)
+genreEntry3.pack()
+genreEntry3.place(x = initialX, y = initialY + 120)
+
+genreInput = genreEntry.get()
+genreInput2 = genreEntry2.get()
+
+
+year = tk.Label(text = "Year:")
+year.pack(ipadx = 10, ipady = 10)
+year.place(x = initialX, y = initialY + 160)
+yearEntry = tk.Entry(fg = "black", bg = "white", width = 30)
+yearEntry.pack() 
+yearEntry.place(x = initialX, y = initialY + 180)
+yearInput = yearEntry.get()
 
 explicit = tk.Label(text = "Explicit:")
 explicit.pack(ipadx = 10, ipady = 10)
-explicit.place(x = initialX, y = initialY + 180)
+explicit.place(x = initialX, y = initialY + 220)
 explicitEntry = tk.Entry(fg = "black", bg = "white", width = 30)
 explicitEntry.pack() 
-explicitEntry.place(x = initialX, y = initialY + 200)
-
-keyword = tk.Label(text = "Keyword:")
-keyword.pack(ipadx = 10, ipady = 10) 
-keyword.place(x = initialX, y= initialY + 240)
-keyWordEntry = tk.Entry(fg = "black", bg = "white", width = 30)
-keyWordEntry.pack() 
-keyWordEntry.place(x = initialX, y = initialY + 260)
-
-language = tk.Label(text = "Language:")
-language.pack(ipadx = 10, ipady = 10)
-language.place(x = initialX, y = initialY + 300)
-languageEntry = tk.Entry(fg = "black", bg = "white", width = 30)
-languageEntry.pack() 
-languageEntry.place(x = initialX, y = initialY + 320)
+explicitEntry.place(x = initialX, y = initialY + 240)
+explicitInput = explicitEntry.get()
 
 director = tk.Label(text = "Director:")
 director.pack(ipadx = 10, ipady = 10)
-director.place (x = initialX, y = initialY + 360)
+director.place (x = initialX, y = initialY + 280)
 directorEntry = tk.Entry(fg = "black", bg = "white", width = 30)
 directorEntry.pack() 
-directorEntry.place(x = initialX, y = initialY + 380)
+directorEntry.place(x = initialX, y = initialY + 300)
+directorInput = directorEntry.get()
+
+button = tk.Button(
+    text="Enter",
+    width=28,
+    height=1,
+    bg="white",
+    fg="black",
+) 
+button.place(x = initialX, y = initialY + 360)
 
 
 window.mainloop()
