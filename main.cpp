0c680b2fcde1d58c3c03e2a12cc514eca278d6d7
mBeVargo <73911733+mBeVargo@users.noreply.github.com>
2022-04-19 23:18:33 -0400

    Update main.cpp
    update - genre vectors now not empty!

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include "graph.cpp"
+#include "graph.h"
 using std::string;
 using std::vector;
 using std::cout;
@@ -30,7 +30,7 @@ int main() {
     getline(f0Stream, inputInit);
     unsigned long count0 = 0;
     while (getline(f0Stream, inputInit)) {
-        if (count0 == 100) {  //change for different range of test cases
+        if (count0 == 4) {  //change for different range of test cases
             break;
         }
         stringstream lineData(inputInit);
@@ -68,7 +68,7 @@ int main() {
     getline(f1Stream, input);
     unsigned long count1 = 0;
     while (getline(f1Stream, input)) {
-        if (count1 == 100) {  //change for different range of test cases
+        if (count1 == 4) {  //change for different range of test cases
             break;
         }
 
@@ -104,15 +104,18 @@ int main() {
             while (tempStr.find(',') != string::npos && temp -> genre.size() < 3) {
                 temp -> genre.push_back(tempStr.substr(0, tempStr.find(',')));  //romance,comedy,somethingelse
                 //cout << "ID: " << temp -> id << " genre: " << tempStr.substr(0, tempStr.find(',')) << endl;
+                //cout << temp -> genre.size() << endl;
                 tempStr = tempStr.substr(tempStr.find(',') + 1);
             }
             temp -> genre.push_back(tempStr.substr(0, string::npos));
-            for (auto v : adjList -> getNodes()) {
+            for (auto &v : adjList -> getNodes()) {
                 if (v.second.id == temp -> id) {
                     v.second.isExplicit = temp -> isExplicit;
-                    for (auto w : temp -> genre) {
-                        cout << "ID: " << temp -> id << " genre: " << w << endl;
+                    for (auto &w : temp -> genre) {
+                        //cout << "ID: " << temp -> id << " genre: " << w << endl;
                         v.second.genre.push_back(w);
+                        //cout << "size of v's genres: " << v.second.genre.size() << endl;
+                        //cout << "ID: " << temp -> id << " first g: " << v.second.genre[0] << endl;
                     }
                 }
                 //v.second.genre = temp -> genre;
@@ -139,7 +142,7 @@ int main() {
     getline(f2Stream, input);
     unsigned long count2 = 0;
     while (getline(f2Stream, input)) {
-        if (count2 == 100) { //change for different range of test cases
+        if (count2 == 4) { //change for different range of test cases
             break;
         }
 
@@ -154,7 +157,7 @@ int main() {
         temp -> director = token.substr(0, ',');
         getline(lineData, token, '\t');
 
-        for (auto v : adjList -> getNodes()) {
+        for (auto &v : adjList -> getNodes()) {
             if (v.second.id == temp -> id) {
                 v.second.director = temp -> director;
             }
@@ -188,11 +191,14 @@ int main() {
 
     for (auto i : adjList -> getNodes()) {
         for (auto j : adjList -> getNodes()) {
+            //cout << "made it" << endl;
+            //cout << i.second.genre.size() << endl;
             adjList -> createEdge(i.second, j.second);
+            //cout << adjList -> getNodes()[i].genre[0] << " " << adjList -> getNodes()[j].genre[0] << endl;
         }
     }
 
-    //adjList -> printGraph();
+    adjList -> printGraph();
     //adjList.dfs(adjList.getNodes()[1]);
 
     cout << "num nodes: " << (adjList -> getGraph()).size() << endl;
@@ -203,7 +209,9 @@ int main() {
         }
     }
     cout << "num edges: " << check << endl;
-
+    //cout << "and upon closer inspection: " << adjList -> getGraph()[0][0].second << endl;
+    //cout << "and upon closer inspection: " << adjList -> getGraph()[1][0].second << endl;
+    //cout << "and upon closer inspection: " << adjList -> getGraph()[2][0].second << endl;
     return 0;
 }
 
