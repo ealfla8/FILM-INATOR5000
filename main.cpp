c4e03a52176413e1b5ca8bcdc264096bb02951ba
ealfla8 <68116219+ealfla8@users.noreply.github.com>
2022-04-20 14:28:37 -0400

    Updated version of main.cpp with movie selection

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include "graph.h"
+#include "graph.cpp"
 using std::string;
 using std::vector;
 using std::cout;
@@ -22,7 +22,7 @@ int main() {
     ifstream f0Stream;
     f0Stream.open(file0);
     if (!(f0Stream.is_open())) {
-        cout << "You twit." << endl;
+        cout << "File Not Opened" << endl;
     }
 
     string inputInit;
@@ -30,7 +30,7 @@ int main() {
     getline(f0Stream, inputInit);
     unsigned long count0 = 0;
     while (getline(f0Stream, inputInit)) {
-        if (count0 == 4) {  //change for different range of test cases
+        if (count0 == 10000) {  
             break;
         }
         stringstream lineData(inputInit);
@@ -41,14 +41,13 @@ int main() {
         temp -> id = stoi(token.substr(2));
         getline(lineData, token, '\t');
         getline(lineData, token, '\t');
-        //temp -> name = token;
+        temp -> name = token;
         getline(lineData, token, '\t');
         getline(lineData, token, '\t');
         if (token == "en" && (adjList -> getGraph().find(temp -> id) == adjList -> getGraph().end())) {
             temp -> genre = {};
             adjList -> insert(*temp);
             count0 += 1;
-            //cout << "just inserted node with ID: " << temp -> id << " and name: " << temp -> name << endl;
         }
         delete temp;
     }
@@ -59,16 +58,15 @@ int main() {
     ifstream f1Stream;
     f1Stream.open(file1);
     if (!(f1Stream.is_open())) {
-        cout << "You twit." << endl;
+        cout << "File Not Opened" << endl;
     }
 
     string input;
 
-    //std::cout << "made it" << std::endl;
     getline(f1Stream, input);
     unsigned long count1 = 0;
     while (getline(f1Stream, input)) {
-        if (count1 == 4) {  //change for different range of test cases
+        if (count1 == 10000) { 
             break;
         }
 
@@ -99,51 +97,41 @@ int main() {
             getline(lineData, token, '\t');
             getline(lineData, token, '\t');
             tempStr = token;
-            //std::cout << "made it 2" << std::endl;
 
             while (tempStr.find(',') != string::npos && temp -> genre.size() < 3) {
                 temp -> genre.push_back(tempStr.substr(0, tempStr.find(',')));  //romance,comedy,somethingelse
-                //cout << "ID: " << temp -> id << " genre: " << tempStr.substr(0, tempStr.find(',')) << endl;
-                //cout << temp -> genre.size() << endl;
                 tempStr = tempStr.substr(tempStr.find(',') + 1);
             }
             temp -> genre.push_back(tempStr.substr(0, string::npos));
             for (auto &v : adjList -> getNodes()) {
                 if (v.second.id == temp -> id) {
                     v.second.isExplicit = temp -> isExplicit;
-                    v.second.name = temp -> name;
                     for (auto &w : temp -> genre) {
-                        //cout << "ID: " << temp -> id << " genre: " << w << endl;
                         v.second.genre.push_back(w);
-                        //cout << "size of v's genres: " << v.second.genre.size() << endl;
-                        //cout << "ID: " << temp -> id << " first g: " << v.second.genre[0] << endl;
                     }
                 }
-                //v.second.genre = temp -> genre;
             }
             count1 += 1;
         }
         delete temp;
     }
 
-    //std::cout << "made it 1" << std::endl;
 
     f1Stream.close();
 
     ifstream f2Stream;
     f2Stream.open(file2);
     if (!(f2Stream.is_open())) {
-        cout << "You twit." << endl;
+        cout << "File Not Opened" << endl;
     }
 
     string input2;
 
-    //std::cout << "made it 2" << std::endl;
 
     getline(f2Stream, input);
     unsigned long count2 = 0;
     while (getline(f2Stream, input)) {
-        if (count2 == 4) { //change for different range of test cases
+        if (count2 == 1000) { 
             break;
         }
 
@@ -170,55 +158,113 @@ int main() {
 
     f2Stream.close();
 
-    /*adjacencyList* adjList = new adjacencyList();
-    node* node1 = new node();
-    node1 -> id = 1;
-    node1 -> name = "One!";
-    node1 -> genre = {"action", "comedy"};
-
-    node* node2 = new node();
-    node2 -> id = 2;
-    node2 -> name = "Two!";
-    node2 -> genre = {"action", "romance"};
-
-    node* node3 = new node();
-    node3 -> id = 3;
-    node3 -> name = "Three!";
-    node3 -> genre = {"romance"};
-
-    adjList -> insert(*node1);
-    adjList -> insert(*node2);
-    adjList -> insert(*node3);*/
-
     for (auto i : adjList -> getNodes()) {
         for (auto j : adjList -> getNodes()) {
-            //cout << "made it" << endl;
-            //cout << i.second.genre.size() << endl;
             adjList -> createEdge(i.second, j.second);
-            //cout << adjList -> getNodes()[i].genre[0] << " " << adjList -> getNodes()[j].genre[0] << endl;
         }
     }
 
-    //adjList -> printGraph();
-
-
-    cout << "num nodes: " << (adjList -> getGraph()).size() << endl;
+ 
     int check = 0;
     for (auto i : adjList -> getGraph()) {
         for (auto j : i.second) {
             check += 1;
         }
     }
-    cout << "num edges: " << check << endl;
 
-    adjList -> printGraph();
+    string userInput = ""; 
+    cout << "Behold! The Filminator 5000!" << endl; 
+    cout << "Choose a movie you like: 1-20" << endl; 
+    cout << "1: Blacksmith Scene" << endl; 
+    cout << "2: The Arrival of the Train" << endl; 
+    cout << "3: The Sea " << endl;  
+    cout << "4: Fishing for Goldfish" << endl; 
+    cout << "5: Jumping the Blanket" << endl; 
+    cout << "6: The Devil's Castle" << endl; 
+    cout << "7: A Terrible Night" << endl; 
+    cout << "8: Card Party" << endl; 
+    cout << "9: The Burglars" << endl; 
+    cout << "10: At the Hypnotist's" << endl; 
+    cout << "11: The Cabinet of Mephistopheles" << endl; 
+    cout << "12: The Funeral of King Christian IX" << endl; 
+    cout << "13: Clowns" << endl; 
+    cout << "14: Hamlet" << endl; 
+    cout << "15: The Cultivation of the Cacao Tree" << endl; 
+    cout << "16: The Death of Ivan the Terrible" << endl; 
+    cout << "17: Love Crazy" << endl; 
+    cout << "18: Les Miserables" << endl; 
+    cout << "19: The Moonshiners" << endl; 
+    cout << "20: The Black Mask" << endl; 
 
-    //cout << adjList -> dfs(adjList -> getNodes()[1]) << endl;
+    cin >> userInput; 
+    node a; 
 
-    //cout << "and upon closer inspection: " << adjList -> getGraph()[0][0].second << endl;
-    //cout << "and upon closer inspection: " << adjList -> getGraph()[1][0].second << endl;
-    //cout << "and upon closer inspection: " << adjList -> getGraph()[2][0].second << endl;
+    if(userInput == "1"){
+         a = adjList->getNodes()[5]; 
+    }
+    if(userInput == "2"){
+         a = adjList->getNodes()[12]; 
+    } 
+    if (userInput == "3"){
+         a = adjList->getNodes()[23]; 
+    } 
+    if(userInput == "4"){
+         a = adjList->getNodes()[28]; 
+    }
+    if (userInput == "5"){
+         a = adjList->getNodes()[31]; 
+    }
+    if(userInput == "6"){
+        a  = adjList->getNodes()[91]; 
+    }
+    if(userInput == "7"){
+        a  = adjList->getNodes()[131]; 
+    }
+    if(userInput == "8"){
+        a  = adjList->getNodes()[132]; 
+    }
+    if(userInput == "9"){
+        a  = adjList->getNodes()[143]; 
+    }
+    if(userInput == "10"){
+        a  = adjList->getNodes()[144]; 
+    }
+    if(userInput == "11"){
+        a = adjList->getNodes()[159]; 
+    }
+    if(userInput == "12"){
+        a = adjList->getNodes()[543]; 
+    }
+    if(userInput == "13"){
+        a = adjList->getNodes()[558]; 
+    }
+    if(userInput == "14"){
+        a = adjList->getNodes()[631]; 
+    }
+    if(userInput == "15"){
+        a = adjList->getNodes()[838]; 
+    }
+    if(userInput == "16"){
+        a = adjList->getNodes()[844]; 
+    }
+    if(userInput == "17"){
+        a = adjList->getNodes()[941]; 
+    }
+    if(userInput == "18"){
+        a = adjList->getNodes()[969]; 
+    }
+    if(userInput == "19"){
+        a = adjList->getNodes()[618]; 
+    }
+    if(userInput == "20"){
+        a = adjList->getNodes()[573]; 
+    }
+
+    adjList->bfs(a); 
+    cout << endl; 
+    adjList->dfs(a); 
+
+
     return 0;
 }
-
 
